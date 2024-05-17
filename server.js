bd9ffb85093ca48a140950a17fe65751fc3399c8
DenysVM <muratov.d@gmail.com>
2024-05-17 14:27:20 +0200

    Cors policy added

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+// server/server.js
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 
 dotenv.config();
@@ -9,6 +11,24 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+
+const allowedOrigins = [
+  'http://localhost:3000', 
+  'https://denysvm.github.io/bakery-pet/', 
+  'https://bakery-pet-backend.onrender.com' 
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
+}));
 
 app.use('/api/auth', authRoutes);
 
