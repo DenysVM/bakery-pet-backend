15dc5f6a00deb8759dbddc0c3d0a44b8fb4cfc03
DenysVM <muratov.d@gmail.com>
2024-05-23 16:56:01 +0200

    - User Model:
    Updated the regular expression for phone number validation to accept
    numbers with + and () symbols.
    
    - Authentication Controllers (authController.js):
    
    Improved error handling and sending appropriate messages to the client
    during user registration and authentication.

--- models/User.js
+++ models/User.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema({
   phone: {
     type: String,
     required: true,
-    match: [/^\d{10}$/, 'is invalid'],
+    match: [/^\d+$/, 'is invalid'], // Изменено для поддержки форматов телефонов, содержащих символы + и ()
   },
   address: {
     street: { type: String, required: true },
