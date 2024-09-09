b68538d4ca8ed976268d51cc831de5dd901088aa
DenysVM <muratov.d@gmail.com>
2024-09-09 12:30:51 +0200

    Admin routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const { registerUser, authUser } = require('../controllers/authController');
 const protect = require('../middleware/authMiddleware');
+const admin = require('../middleware/adminMiddleware');
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', authUser);
 router.get('/profile', protect, (req, res) => {
   res.json(req.user);
+});
+
+router.get('/admin', protect, admin, (req, res) => {
+  res.json({ message: 'Welcome to the admin panel' });
 });
 
 module.exports = router;
