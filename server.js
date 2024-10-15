c94f6f1ed4b410fffb0884964ebe2c7ec969bfad
DenysVM <muratov.d@gmail.com>
2024-10-15 18:01:28 +0200

    Dinamic baseURL for development mode

--- server.js
+++ server.js
@@ -3,33 +3,32 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
-const orderRoutes = require('./routes/orderRoutes'); 
-const productRoutes = require('./routes/productRoutes'); 
+const orderRoutes = require('./routes/orderRoutes');
+const productRoutes = require('./routes/productRoutes');
 
 dotenv.config();
 connectDB();
 
 const app = express();
-
 app.use(express.json());
 
 const allowedOrigins = [
   'http://localhost:3000', 
   'https://denysvm.github.io/bakery-pet',
-  'https://denysvm.github.io',  
-  'https://bakery-pet-backend.onrender.com',
-  'http://172.20.10.6:3000',
-  'http://172.20.10.6:5000'  
+  'https://denysvm.github.io',
+  'https://bakery-pet-backend.onrender.com'
 ];
 
 app.use(cors({
   origin: function (origin, callback) {
     if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
+
+    if (allowedOrigins.indexOf(origin) !== -1 || /^http:\/\/172\.\d+\.\d+\.\d+(:\d+)?$/.test(origin) || /^http:\/\/192\.168\.\d+\.\d+(:\d+)?$/.test(origin)) {
+      return callback(null, true);
+    } else {
       const msg = 'The CORS policy for this site does not allow access from the specified origin.';
       return callback(new Error(msg), false);
     }
-    return callback(null, true);
   },
   credentials: true,
 }));
@@ -39,6 +38,6 @@ app.use('/api/orders', orderRoutes);
 app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
